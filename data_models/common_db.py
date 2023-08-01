7d8d8f9def9c79d6022a863b3e885e51551643c7
Yijun Lin <lin00786@umn.edu>
2023-08-02 00:29:49 +0800

    remove info

--- data_models/common_db.py
+++ data_models/common_db.py
@@ -3,8 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-connection_string = 'postgresql+psycopg2://{usr}:{pwd}@jonsnow.usc.edu/air_quality_dev'\
-    .format(usr='yijun', pwd='m\\tC7;cc')
+connection_string = 'postgresql+psycopg2://{usr}:{pwd}@{server_name}/air_quality_dev'\
+    .format(usr='', pwd='')
 
 engine = create_engine(connection_string, echo=False)
 Session = sessionmaker(bind=engine, expire_on_commit=False)
